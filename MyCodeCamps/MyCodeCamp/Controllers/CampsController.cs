676fcf795a5f233332755409789b3a2774201919
Muhammad Ihsan <mihsan191@gmail.com>
2017-06-28 15:56:09 +1000

    Add actions to return all camps

--- MyCodeCamps/MyCodeCamp/Controllers/CampsController.cs
+++ MyCodeCamps/MyCodeCamp/Controllers/CampsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyCodeCamp.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,19 @@ namespace MyCodeCamp.Controllers
     [Route("api/[controller]")]
     public class CampsController : Controller
     {
+        private ICampRepository _repo;
+
+        public CampsController(ICampRepository repo)
+        {
+            _repo = repo;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Ihsan");
+            var camps = _repo.GetAllCamps();
+
+            return Ok(camps);
         }
     }
 }
