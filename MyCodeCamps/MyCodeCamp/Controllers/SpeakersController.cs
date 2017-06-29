1ad1bd1ffbfd54372b8ffc3b6d355d6135739268
Muhammad Ihsan <mihsan191@gmail.com>
2017-06-29 13:34:07 +1000

    Provide association routes to get speakers for camp

---
+++ MyCodeCamps/MyCodeCamp/Controllers/SpeakersController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyCodeCamp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/camps/{campId}/speakers")]
+    public class SpeakersController : BaseController
+    {
+        private ICampRepository _repository;
+        private ILogger<SpeakersController> _logger;
+        private IMapper _mapper;
+
+        public SpeakersController(ICampRepository repository, ILogger<SpeakersController> logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int campId)
+        {
+            var speakers = _repository.GetSpeakersByCampId(campId);
+
+            return Ok(speakers);
+        }
+    }
+}
